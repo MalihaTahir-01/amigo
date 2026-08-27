f29c987b3804d64f414b4744b7ee003429f13701
Maliha tahir <malihatahir2007@gmail.com>
2026-08-27 10:44:53 +0500

    bump cache to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'amigo-v6';
+const CACHE = 'amigo-v7';
 const ASSETS = ['/', '/index.html', '/app.js', '/auth.js', '/style.css', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
